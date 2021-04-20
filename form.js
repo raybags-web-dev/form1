c6117a7324aeb715e0ad0f4f60f91816155f4789
raybags <baguma.github@gmail.com>
2021-04-20 10:36:41 +0200

    form debugging

--- form.js
+++ form.js
@@ -39,7 +39,6 @@ $(function () {
     });
     $.ajax({
       type: "POST",
-      url: "handler.php",
       data: $form.serialize(),
       success: after_form_submitted,
       dataType: "json",
